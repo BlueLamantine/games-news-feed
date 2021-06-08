11a8ba91b93f64f30f8bc30dc60344edaf1e97a4
Viktoriia <urban.03al03@gmail.com>
2021-06-08 22:56:20 +0300

    v1.5.1 component checkbox fixed

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './Checkbox.css';
 
-export default function Checkbox({ id = null, label = '', onChange = null, condition = false }) {
+export default function Checkbox({
+  id = null,
+  label = '',
+  onChange = () => {
+    return false;
+  },
+  condition = false,
+}) {
   return (
     <>
       <div className={styles.checkbox}>
-        <input type="checkbox" id={id} value={id} defaultChecked={condition} onChange={onChange} />
+        <input type="checkbox" id={id} value={id} onChange={onChange} checked={condition} />
         <label htmlFor={id}></label>
         <div className={styles.label_text}>{label}</div>
       </div>
