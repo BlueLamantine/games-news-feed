fabca4c7fe1ac53a0904439cbc465fbae8cfe557
Viktoriia <urban.03al03@gmail.com>
2021-06-10 23:29:06 +0300

    searchbar modified

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './SearchBar.css';
 
 export default function Search({ value, handleSearch, currentTag }) {
-  let input = React.createRef();
   return (
     <>
       <div className={styles.search_bar}>
@@ -11,14 +10,12 @@ export default function Search({ value, handleSearch, currentTag }) {
           id="search-input"
           placeholder="Search by keyword"
           defaultValue={value}
-          ref={input}
           onBlur={event => handleSearch(event.target.value)}
           onKeyPress={event => {
             if (event.key === 'Enter') {
               handleSearch(event.target.value);
             }
           }}
-          disabled={currentTag != null || false}
         />
       </div>
     </>
